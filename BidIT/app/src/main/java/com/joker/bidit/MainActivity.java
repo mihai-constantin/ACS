10e0325c9592b9ba1c917b27d44065f838215ee7
Mihai Constantin <mihai.constantin98@stud.acs.upb.ro>
2020-03-31 23:54:10 +0300

    add simple firebase authentication

--- BidIT/app/src/main/java/com/joker/bidit/MainActivity.java
+++ BidIT/app/src/main/java/com/joker/bidit/MainActivity.java
@@ -2,23 +2,37 @@ package com.joker.bidit;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.joker.bidit.dashboard.ProductsActivity;
 import com.joker.bidit.login.Authentication;
 import com.joker.bidit.utils.EmailHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mEditTextEmail;
     private EditText mEditTextPhone;
     private CheckBox mCheckBoxAccept;
 
     private Authentication mAuthentication;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "SignInActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,18 @@ public class MainActivity extends AppCompatActivity {
 
         mAuthentication = new Authentication();
         initView();
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        setGooglePlusButtonText(findViewById(R.id.sign_in_button), getString(R.string.continueWithGoogle));
     }
 
     // get the views from the layout based on an unique id defined in the xml file
@@ -71,12 +97,75 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     }
+
     public void recyclerViewOnClick(View view) {
         if (isEmailValid() && isPhoneValid() && isAccepted()) {
             Toast.makeText(MainActivity.this, mAuthentication.toString(),
                     Toast.LENGTH_LONG).show();
-            startActivity(new Intent(MainActivity.this, com.joker.bidit.dashboard.ProductsActivity.class));
+            startActivity(new Intent(MainActivity.this, ProductsActivity.class));
         }
     }
 
+    protected void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setText(buttonText);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(MainActivity.this,
+                    String.format("Hello, %s", account.getDisplayName()),
+                    Toast.LENGTH_LONG).show();
+
+
+            startActivity(new Intent(MainActivity.this, ProductsActivity.class));
+
+//            updateUI(account);
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+
+//            updateUI(null);
+        }
+    }
 }
