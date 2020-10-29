dc4f7791de37a9598867ea33ee4af576ca4d0178
Mihai Constantin <mihai.constantin98@gmail.com>
2020-10-29 10:18:06 +0200

    adaugare exemple de rulare lab04

--- lab04/number_of_digits.c
+++ lab04/number_of_digits.c
@@ -4,6 +4,12 @@
  */
 #include <stdio.h>
 
+/**
+ *  f(1234) = 1 + f(123) = 1 + 1 + 1 + 1 = 4
+ *  f(123) = 1 + f(12) = 1 + 1 + 1 = 3
+ *  f(12) = 1 + f(1) = 1 + 1 = 2
+ *  f(1) = 1
+ */
 int number_of_digits(int x) {
     if (x < 10) {
         return 1;
--- lab04/warm-up.c
+++ lab04/warm-up.c
@@ -7,6 +7,13 @@ int sum_recursive(int n) {
 	return n % 10 + sum_recursive(n / 10);
 }
 
+/**
+ * f(541) = 1 + f(54) = 1 + 4 + 5 = 10
+ * f(54) = 4 + f(5) = 4 + 5
+ * f(5) = 5 + f(0) = 5 + 0 = 5
+ * f(0) = 0
+ * 
+ */
 int sum_recursive_odd_digits(int n) {
 	if (n < 10) {
 		if (n % 2 == 1) {
@@ -20,6 +27,13 @@ int sum_recursive_odd_digits(int n) {
 	return sum_recursive_odd_digits(n / 10);
 }
 
+/**
+ *	f(54123) = 3 + f(5412) = 3 + 1 + 5
+ *	f(5412) = f(541) = 1 + 5
+ *	f(541) = 1 + f(54) = 1 + 5
+ *	f(54) = f(5) = 5
+ *	f(5) = 5
+ */
 int get_last_odd_digit_recursive(int n) {
 	if (n < 10) {
 		if (n % 2 == 1) {
