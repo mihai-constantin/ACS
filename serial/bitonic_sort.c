aec6a47c003b76146abc3b7f8482db7bc1c7dee6
Mihai Constantin <mihai.constantin98@gmail.com>
2020-11-21 21:36:54 +0200

    make array data and array length global variables

--- serial/bitonic_sort.c
+++ serial/bitonic_sort.c
@@ -6,7 +6,10 @@
 #include <stdbool.h>
 #include <sys/time.h>
 
-void init(int *arr, int n) {
+int *arr; // data array to be sorted
+int n; // data array size
+
+void init() {
     for (int i = 0; i < n; i++) {
         arr[i] = rand() % n;
     }
@@ -18,39 +21,39 @@ void swap(int *a, int *b) {
     *b = temp;
 }
 
-void compareAndSwap(int *arr, int i, int j, int direction) {
+void compareAndSwap(int i, int j, int direction) {
     if ((arr[i] > arr[j]) == direction) {
         swap(&arr[i], &arr[j]);
     }
 }
 
-void bitonic_merge(int *arr, int low, int cnt, int direction) {
+void bitonic_merge(int low, int cnt, int direction) {
     if (cnt == 1) {
         return;
     }
     int k = cnt/2;
     for (int i = low; i < low + k; i++) {
-        compareAndSwap(arr, i, i + k, direction);
+        compareAndSwap(i, i + k, direction);
     }
-    bitonic_merge(arr, low, k, direction);
-    bitonic_merge(arr, low + k, k, direction);
+    bitonic_merge(low, k, direction);
+    bitonic_merge(low + k, k, direction);
 }
 
-void bitonic_sort(int *arr, int low, int cnt, int direction) {
+void bitonic_sort(int low, int cnt, int direction) {
     if (cnt == 1) {
         return;
     }
     int k = cnt/2;
-    bitonic_sort(arr, low, k, 1); // ascending order
-    bitonic_sort(arr, low + k, k, 0); // descending order
-    bitonic_merge(arr, low, cnt, direction); // merge
+    bitonic_sort(low, k, 1); // ascending order
+    bitonic_sort(low + k, k, 0); // descending order
+    bitonic_merge(low, cnt, direction); // merge
 }
 
-void sort(int *arr, int n, int up) {
-    bitonic_sort(arr, 0, n, up);
+void sort(int up) {
+    bitonic_sort(0, n, up);
 }
 
-void test(int *arr, int n) {
+void test() {
     bool pass = true;
     for (int i = 1; i < n; i++) {
         pass &= (arr[i - 1] <= arr[i]);
@@ -61,23 +64,23 @@ void test(int *arr, int n) {
 int main(int argc, char const *argv[])
 {
     if (argc != 2) {
-        printf("Usage: %s <array length>\n", argv[0]);
+        printf("Usage: %s 2^<array length>\n", argv[0]);
         exit(1);
     }
 
-    int n = atoi(argv[1]);
-    int* arr = (int*) malloc(n * sizeof(int));
+    n = (1 << atoi(argv[1]));
+    arr = (int*) malloc(n * sizeof(int));
 
     struct timeval startwtime, endwtime;
     double seq_time;
 
-    init(arr, n);
+    init();
     gettimeofday(&startwtime, NULL);
-    sort(arr, n, 1); // sort the array in ascending order
+    sort(1); // sort the array in ascending order
     gettimeofday(&endwtime, NULL);
     seq_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6 + endwtime.tv_sec - startwtime.tv_sec);
     printf("Time taken = %f\n", seq_time);
-    test(arr, n);
+    test();
     free(arr);
 
     return 0;
