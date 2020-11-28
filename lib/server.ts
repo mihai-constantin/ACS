cd8926db91eb8534707d78b66dc72d66c96f324d
Mihai Constantin <mihai.constantin98@gmail.com>
2020-11-28 22:01:33 +0200

    formatare 2 spatii

--- lib/server.ts
+++ lib/server.ts
@@ -3,5 +3,5 @@ import app from "./config/app";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-   console.log('Express server listening on port ' + PORT);
+  console.log('Express server listening on port ' + PORT);
 });
