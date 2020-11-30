704853ac79c18607509bcf79dc471bf7997e4638
raresionut1 <rares.constantin@stud.acs.upb.ro>
2020-11-30 18:24:12 +0200

    Implemented the parallel algorithm using OpenMP

--- openmp/README.md
+++ openmp/README.md
@@ -1,13 +1,23 @@
 # Bitonic Sort - varianta paralela OpenMP
 
 ## Detalii implementare
-* TODO
-        
+* Deoarece '#pragma omp parallel' realizeaza un numar mare de fork-uri si join-uri, este
+important sa stim cand trebuie sa folosim functia de sortare paralela a
+secventelor si cand trebuie sa o folosim pe cea seriala (pentru a nu se
+crea un overhead care nu este necesar).
+    * Atunci cand suntem deja in interiorul unei secvente de cod
+    paralelizata cu '#pragma omp parallel' si trebuie sa apelam functia de sortare
+    a unei secvente din vector, vom alege functia secventiala (deoarece toate
+    core-urile sunt deja ocupate cu alte secvente)
+    * Atunci cand suntem intr-o secventa de cod seriala, putem apela metoda de
+      sortare a secventelor paralela, deoarece core-urile nu sunt ocupate cu
+      alte computatii
+
 ## Timp rulare
 * In urma rularii de un numar semnificativ de ori, s-au obtinut urmatoarele rezultate:
-    * pentru un vector de dimensiune 2^20, o medie de TODO
-    * pentru un vector de dimensiune 2^21, o medie de TODO
-    * pentru un vector de dimensiune 2^22, o medie de TODO
-    * pentru un vector de dimensiune 2^23, o medie de TODO
-    * pentru un vector de dimensiune 2^24, o medie de TODO
-    * pentru un vector de dimensiune 2^25, o medie de TODO
+    * dimensiune vector: 2^20 | timp mediu de executie: TODO | speedup: TODO
+    * dimensiune vector: 2^21 | timp mediu de executie: TODO | speedup: TODO
+    * dimensiune vector: 2^22 | timp mediu de executie: TODO | speedup: TODO
+    * dimensiune vector: 2^23 | timp mediu de executie: TODO | speedup: TODO
+    * dimensiune vector: 2^24 | timp mediu de executie: TODO | speedup: TODO
+    * dimensiune vector: 2^25 | timp mediu de executie: TODO | speedup: TODO
--- openmp/bitonic_sort_openmp.c
+++ openmp/bitonic_sort_openmp.c
@@ -1,12 +1,14 @@
-// Bitonic Sort - Sequential implementation
+// Bitonic Sort - arruential implementation
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <sys/time.h>
+#include <omp.h>
 
 int *arr; // data array to be sorted
 int n; // data array size
+int p; // number of threads
 
 const int ASCENDING = 1;
 const int DESCENDING = 0;
@@ -29,30 +31,73 @@ void compare(int i, int j, int direction) {
     }
 }
 
-void bitonic_merge(int low, int cnt, int direction) {
-    if (cnt == 1) {
+void bitonic_sort_seq(int start, int length, int direction)
+{
+    int i;
+    int split_length;
+
+    if (length == 1)
         return;
+
+    if (length % 2 !=0 )
+    {
+        printf("error\n");
+        exit(0);
     }
-    int k = cnt/2;
-    for (int i = low; i < low + k; i++) {
-        compare(i, i + k, direction);
-    }
-    bitonic_merge(low, k, direction);
-    bitonic_merge(low + k, k, direction);
+
+    split_length = length / 2;
+
+    // bitonic split
+    for (i = start; i < start + split_length; i++)
+        compare(i, i + split_length, direction);
+
+    bitonic_sort_seq(start, split_length, direction);
+    bitonic_sort_seq(start + split_length, split_length, direction);
 }
 
-void bitonic_sort(int low, int cnt, int direction) {
-    if (cnt == 1) {
+void bitonic_sort_par(int start, int length, int direction) {
+    int i;
+    int split_length;
+    int m = n / p;
+
+    if (length == 1)
         return;
+
+    if (length % 2 !=0 ) {
+        exit(0);
     }
-    int k = cnt/2;
-    bitonic_sort(low, k, 1); // ascending order
-    bitonic_sort(low + k, k, 0); // descending order
-    bitonic_merge(low, cnt, direction); // merge
+
+    split_length = length / 2;
+
+    // bitonic split
+    #pragma omp parallel for shared(arr, direction, start, split_length) private(i)
+    for (i = start; i < start + split_length; i++)
+        compare(i, i + split_length, direction);
+
+    if (split_length > m) {
+        bitonic_sort_par(start, split_length, direction);
+        bitonic_sort_par(start + split_length, split_length, direction);
+    }
 }
 
-void sort(int order_type) {
-    bitonic_sort(0, n, order_type);
+void sort(int direction) {
+  int i, j, m = n / p;
+  // make bitonic sequence - part 1
+  for (i = 2; i <= m; i = 2 * i) {
+      #pragma omp parallel for shared(i, arr) private(j, direction)
+      for (j = 0; j < n; j += i)
+          bitonic_sort_seq(j, i, (j / i) % 2 == 0 ? ASCENDING : DESCENDING);
+  }
+
+  // make bitonic sequence - part 2
+  for (i = 2; i <= p; i = 2 * i) {
+      for (j = 0; j < p; j += i)
+          bitonic_sort_par(j*m, i*m,  (j / i) % 2 == 0 ? ASCENDING : DESCENDING);
+
+      #pragma omp parallel for shared(j)
+      for (j = 0; j < p; j++)
+          bitonic_sort_seq(j*m, m,  (j / i) % 2 == 0 ? ASCENDING : DESCENDING);
+  }
 }
 
 void test() {
@@ -73,15 +118,19 @@ int main(int argc, char const *argv[])
     n = (1 << atoi(argv[1]));
     arr = (int*) malloc(n * sizeof(int));
 
+    // OpenMP - get number of threads
+    p = 4;
+    omp_set_num_threads(4);
+
     struct timeval startwtime, endwtime;
-    double seq_time;
+    double arr_time;
 
     init();
     gettimeofday(&startwtime, NULL);
     sort(ASCENDING); // sort the array in ascending order
     gettimeofday(&endwtime, NULL);
-    seq_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6 + endwtime.tv_sec - startwtime.tv_sec);
-    printf("Time taken = %f\n", seq_time);
+    arr_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6 + endwtime.tv_sec - startwtime.tv_sec);
+    printf("Time taken = %f\n", arr_time);
     test();
     free(arr);
 
