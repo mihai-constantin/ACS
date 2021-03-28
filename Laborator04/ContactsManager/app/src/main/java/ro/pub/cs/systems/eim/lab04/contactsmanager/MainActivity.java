8c6daa1062e1fe3a700cab1408b1e9d5efbfdc14
Mihai Constantin <mihai.constantin98@gmail.com>
2021-03-28 14:43:28 +0300

    restructure

---
+++ Laborator04/ContactsManager/app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.lab04.contactsmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
