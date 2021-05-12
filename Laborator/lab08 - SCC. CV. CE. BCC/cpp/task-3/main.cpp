02635448961412af75ea5b23334c8126c4fe19f8
Mihai Constantin <mihai.constantin98@gmail.com>
2021-05-12 15:16:47 +0300

    tarjan - cut edges

--- Laborator/lab08 - SCC. CV. CE. BCC/cpp/task-3/main.cpp
+++ Laborator/lab08 - SCC. CV. CE. BCC/cpp/task-3/main.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 class Task {
@@ -29,6 +31,14 @@ private:
     // exemplu: daca adj[node] = {..., neigh, ...} => exista arcul (node, neigh)
     vector<int> adj[NMAX];
 
+    vector<int> idx;
+    vector<int> lowlink;
+
+    vector<int> visited;
+    vector<int> parent;
+
+    int indecs;
+
     void read_input() {
         ifstream fin("in");
         fin >> n >> m;
@@ -40,6 +50,35 @@ private:
         fin.close();
     }
 
+    void tarjan(vector<Edge> &sol, int x) {
+        int i, y;
+        Edge edge;
+        
+        idx[x] = lowlink[x] = indecs++;
+        visited[x] = 1;
+
+        // parcurg vecinii y ai lui x
+        for (i = 0; i < adj[x].size(); i++) {
+            y = adj[x][i];
+
+            if (!visited[y]) {
+                parent[y] = x;
+                tarjan(sol, y);
+
+                lowlink[x] = min(lowlink[x], lowlink[y]);
+
+                if (lowlink[y] > idx[x]) {
+                    //  cout << "edge: " << x << " " << y << '\n';
+                    edge.x = x;
+                    edge.y = y;
+                    sol.push_back(edge);
+                }
+            } else if (parent[x] != y) {
+                lowlink[x] = min(lowlink[x], idx[y]);
+            }
+        }
+    }
+
     vector<Edge> get_result() {
         //
         // TODO: Gasiti toate muchiile critice ale grafului neorientat stocat cu liste de adiacenta in adj.
@@ -49,7 +88,44 @@ private:
         // Indicație: Folosiți algoritmul lui Tarjan pentru CE.
         //
 
+        int i;
         vector<Edge> all_ces;
+
+        idx.resize(n + 1);
+        lowlink.resize(n + 1);
+
+        visited.resize(n + 1);
+        visited.assign(n + 1, 0);
+
+        parent.resize(n + 1);
+        parent.assign(n + 1, -1);
+
+        tarjan(all_ces, 1);
+
+        // cout << "idx\n";
+        // for (i = 1; i <= n; i++) {
+        //     cout << idx[i] << " ";
+        // }
+        // cout << '\n';
+
+        // cout << "lowlink\n";
+        // for (i = 1; i <= n; i++) {
+        //     cout << lowlink[i] << " ";
+        // }
+        // cout << '\n';
+
+        // cout << "parent\n";
+        // for (i = 1; i <= n; i++) {
+        //     cout << parent[i] << " ";
+        // }
+        // cout << '\n';
+
+        // cout << "visited\n";
+        // for (i = 1; i <= n; i++) {
+        //     cout << visited[i] << " ";
+        // }
+        // cout << '\n';
+
         return all_ces;
     }
 
