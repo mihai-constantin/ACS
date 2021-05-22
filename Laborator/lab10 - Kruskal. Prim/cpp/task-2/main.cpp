61a682f7b0dc85bf0ca37efff0acd83744577581
Mihai Constantin <mihai.constantin98@gmail.com>
2021-05-22 14:08:14 +0300

    prim

--- Laborator/lab10 - Kruskal. Prim/cpp/task-2/main.cpp
+++ Laborator/lab10 - Kruskal. Prim/cpp/task-2/main.cpp
@@ -1,4 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
 using namespace std;
 
 // numarul maxim de noduri
@@ -18,6 +21,7 @@ private:
     // adj[i] = lista de adiacenta a lui i
     // (j, w) - exista muchie de la i la j de cost w
     vector<pair<int, int>> adj[NMAX];
+    priority_queue<pair<int, int>> H;
 
     void read_input() {
         ifstream fin("in");
@@ -35,7 +39,52 @@ private:
         // TODO: Calculati costul minim al unui arbore de acoperire
         // folosind algoritmul lui Prim.
         //
-        return 0;
+
+        int i, x, y, cost;
+
+        vector<int> d(n + 1, NMAX);
+        vector<int> visited(n + 1, 0);
+        vector<int> parent(n + 1, -1);
+
+        // introduc in heap nodul de start
+        H.push(make_pair(0, 1));
+        d[1] = 0;
+
+        while(!H.empty()) {
+
+            while(!H.empty() && visited[H.top().second]) {
+                H.pop();
+            }
+
+            if (H.empty()) {
+                break;
+            }
+
+            x = H.top().second;
+            visited[x] = 1;
+
+            // parcurg vecinii y ai lui x
+            for (i = 0; i < adj[x].size(); i++) {
+                y = adj[x][i].first;
+                cost = adj[x][i].second;
+
+                if (d[y] > cost && !visited[y]) {
+                    parent[y] = x;
+                    d[y] = cost;
+                    H.push(make_pair(-d[y], y));
+                }
+            }
+        }
+
+        cost = 0;
+        for (int i = 1; i <= n; i++) {
+            // printf("[%d] %d %d\n", i, d[i], parent[i]);
+            if (parent[i] != -1) {
+                cost += d[i];
+            }
+        }
+
+        return cost;
     }
 
     void print_output(int result) {
