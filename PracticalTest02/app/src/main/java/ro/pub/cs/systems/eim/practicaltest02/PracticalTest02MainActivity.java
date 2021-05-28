3b4cabea9e6b1d1d3f82184e7de6491fa43f49e3
Mihai Constantin <mihai.constantin98@gmail.com>
2021-05-28 13:07:28 +0300

    add skel

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+    }
+}
